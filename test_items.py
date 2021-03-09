8b887c6acd8ad2e041b1df08d8428ac1ef04ea6c
wai1er <Jdane@mail.ru>
2021-03-10 00:34:49 +0300

    The solution added

--- test_items.py
+++ test_items.py
@@ -2,4 +2,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_should_exist(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, "Кнопка не найдена"
